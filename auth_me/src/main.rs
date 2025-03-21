0b317697fb4c70eae074754c2c7d7df56fe89a42
Tayon <tayondw@gmail.com>
2025-03-21 17:47:56 -0400

    will delete, try again

--- auth_me/src/main.rs
+++ auth_me/src/main.rs
@@ -1,3 +1,18 @@
+use diesel::prelude::*;
+use dotenvy::dotenv;
+mod db;
+mod config;
+use config::Config;
+
 fn main() {
-    println!("Hello, world!");
+    // Load .env file
+    dotenv().ok();
+
+    // Access environment variables
+    let config: Config = Config::new().expect("Failed to load configuration");
+
+    // Pass the database_url to establish_connection
+    let connection = db::establish_connection(&config.database_url);
+
+    // Rest of your code...
 }
